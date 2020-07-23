3d945f6b3b82e6b439bf9ba334fcd2224ef0fad6
Mingfa Ma <mingfa.ma'deepnetsecurity.com>
2020-07-23 10:42:52 +0100

    actually need to accept text/html, as this response id not default json

--- dualshield/s1_sso_password.js
+++ dualshield/s1_sso_password.js
@@ -8,7 +8,6 @@ export default function () {
   response = http.get("https://dualshield6.bletchley16.com:8073/dac/app?ep=/",
         {
           headers: {
-            "upgrade-insecure-requests": "1",
             "user-agent":
               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36",
             accept:
@@ -16,10 +15,6 @@ export default function () {
             "sec-fetch-site": "same-origin",
             "sec-fetch-mode": "navigate",
             "sec-fetch-dest": "document",
-            referer: "https://dualshield6.deepnetsecurity.com/dac/",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language":
-              "en-GB,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6",
           },
         });
   sleep(1);
